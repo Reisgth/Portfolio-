06b824bb54a2aa57c9a81f5e36d535a305549b08
Gustavo Henrique dos Reis <61255621+Reisgth@users.noreply.github.com>
2023-06-12 00:58:37 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,81 @@
+function writeTitle(){
+    function activeWorld(element){
+        const arrText = element.innerHTML.split('');
+        element.innerHTML = '';
+        arrText.forEach((letra, i) =>{
+            setTimeout(()=>{
+                element.innerHTML += letra;
+            }, 75 * i);
+        })
+    }
+    const title = document.querySelector('.digitando');
+    activeWorld(title);
+}
+writeTitle();
+
+function aboutMe(){
+    const divExperience = document.querySelectorAll('.experience_content div'); 
+    const liExperience = document.querySelectorAll('.experience_content ul li'); 
+    const divEducation = document.querySelectorAll('.education_content div'); 
+    const liEducation = document.querySelectorAll('.education_content ul li'); 
+
+    divEducation[0].classList.add('active');
+    liEducation[0].classList.add('active');
+    divExperience[0].classList.add('active');
+    liExperience[0].classList.add('active');
+    
+    function slideShow(index){
+        divExperience.forEach((div)=>{
+            div.classList.remove('active')
+        });
+        liExperience.forEach((buttom)=>{
+            buttom.classList.remove('active');
+        })
+        divExperience[index].classList.add('active');
+        liExperience[index].classList.add('active');
+    }
+    
+    function slideShow2(index){
+        divEducation.forEach((div)=>{
+            div.classList.remove('active')
+        });
+        liEducation.forEach((buttom)=>{
+            buttom.classList.remove('active');
+        })
+        divEducation[index].classList.add('active');
+        liEducation[index].classList.add('active');
+    }
+    
+    liExperience.forEach((event, index) => {
+        event.addEventListener('click', () =>{
+            slideShow(index);
+        })
+    });
+    
+    liEducation.forEach((event, index) => {
+        event.addEventListener('click', () =>{
+            slideShow2(index);
+        })
+    });
+}
+
+aboutMe();
+
+const listAll = document.querySelectorAll('.projects_storage ul li');
+const buttomGeral = document.querySelectorAll('.projects_models ul li');
+const buttomAll = document.querySelectorAll('.projects_models .all');
+
+
+function removeClick(index){
+    buttomGeral.forEach((item)=>{
+        item.classList.remove('.active');
+    });
+    buttomGeral[index].classList.add('.active')
+}
+
+buttomGeral.forEach((item, index)=>{
+    item.addEventListener('click',()=>{
+        removeClick(index);
+    })
+    
+})
